e03338f0f9e6458d12a07410b5a20fb1585def9e
Lionel Manje <manjetang@yahoo.com>
2018-10-25 20:11:28 -0700

    Updates

---
+++ precache-manifest.ccbf605e862059a6d349af59f9b5545f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "b0b7f617a43cc163aaee",
+    "url": "/clicky-game/static/js/main.b0b7f617.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/clicky-game/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "b0b7f617a43cc163aaee",
+    "url": "/clicky-game/static/css/main.22d1365d.chunk.css"
+  },
+  {
+    "revision": "415bedc0ee98e5c5222c0e7e3e678851",
+    "url": "/clicky-game/index.html"
+  }
+];
